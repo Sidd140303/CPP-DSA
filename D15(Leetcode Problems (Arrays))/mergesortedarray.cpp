a30c65956f003e3c0a187085fe09c7b7afbb5119
Siddharth Gupta <siddharth140303@gmail.com>
2024-05-25 01:54:36 +0530

    made changes

--- D15(Leetcode Problems (Arrays))/mergesortedarray.cpp
+++ D15(Leetcode Problems (Arrays))/mergesortedarray.cpp
@@ -10,25 +10,21 @@ vector<int> merge(vector<int> arr1, vector<int> arr2)
     {
         if (arr1[s1] < arr2[s2])
         {
-            ans.push_back(arr1[s1]);
-            s1++;
+            ans.push_back(arr1[s1++]);
         }
         else
         {
-            ans.push_back(arr2[s2]);
-            s2++;
+            ans.push_back(arr2[s2++]);
         }
     }
 
     while (s1 < arr1.size())
     {
-        ans.push_back(arr1[s1]);
-        s1++;
+        ans.push_back(arr1[s1++]);
     }
     while (s2 < arr2.size())
     {
-        ans.push_back(arr2[s2]);
-        s2++;
+        ans.push_back(arr2[s2++]);
     }
     return ans;
 }
